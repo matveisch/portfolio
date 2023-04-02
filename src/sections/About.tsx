983704c7f7ab9877e9139e719587485ca0a464ed
Matvei <matvesch@protonmail.com>
2023-04-02 10:39:33 +0300

    added great about me description

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,26 @@
 export default function About() {
   return (
     <section className="py-[100px]">
-      <h1>About Me</h1>
+      <h1 className="text-3xl text-slate-300">
+        About <span className="text-cyan-300">Me</span>
+      </h1>
+      <div>
+        <p>
+          Hi there! My name is Matvei, and I'm thrilled to share a little bit about myself with you. I'm a web developer
+          with a passion for creating beautiful and functional digital experiences. My journey in web development began
+          in 2020, and since then, I've had the privilege of working with a diverse range of clients, from small
+          businesses to larger corporations. Currently, I work as a freelancer, and I love the variety of projects that
+          come my way.
+          <br /> <br />
+          In the past, I worked with a student-led web studio where I honed my skills in building user-centric websites.
+          I also had the opportunity to work with an Israeli start-up where I learned how to work in a fast-paced, agile
+          environment.
+          <br /> <br />
+          Overall, I'm a dedicated and passionate web developer who is committed to creating engaging, user-friendly
+          experiences on the web. Thank you for taking the time to learn more about me, and I hope to hear from you
+          soon!
+        </p>
+      </div>
     </section>
   );
 }

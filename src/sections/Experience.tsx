95c7f0f0a3d0c85f70ce008f7aa279a92604b968
Matvei <matvesch@protonmail.com>
2023-04-03 14:30:56 +0300

    tab animation

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import JobDescription from '@/components/JobDescription';
 
 export default function Experience() {
   const [activeTab, setActiveTab] = useState(0);
+  const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0);
+  const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [tabUnderlineTop, setTabUnderlineTop] = useState(0);
+  const tabsRef = useRef<HTMLButtonElement[]>([]);
   const jobs = [
     {
       title: 'Front-End Developer',
@@ -30,25 +35,66 @@ export default function Experience() {
     },
   ];
 
+  useEffect(() => {
+    function handleScreenResize() {
+      setScreenWidth(window.innerWidth);
+    }
+    handleScreenResize();
+
+    window.addEventListener('resize', handleScreenResize);
+
+    return () => {
+      window.removeEventListener('resize', handleScreenResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    function setTabPosition() {
+      const currentTab: HTMLButtonElement = tabsRef.current[activeTab];
+
+      setTabUnderlineLeft(currentTab?.offsetLeft ?? 0);
+      setTabUnderlineTop(currentTab?.offsetTop ?? 0);
+      setTabUnderlineWidth(currentTab?.clientWidth ?? 0);
+    }
+
+    setTabPosition();
+    window.addEventListener('resize', setTabPosition);
+
+    return () => window.removeEventListener('resize', setTabPosition);
+  }, [activeTab]);
+
   return (
     <section className="pb-[200px]">
       <h1 className="text-3xl text-slate-300 mb-10">
         Where <span className="text-cyan-300">I've Worked</span>
       </h1>
       <div className="flex gap-10 flex-col sm:flex-row min-h-[250px]">
-        <div className="flex flex-row sm:flex-col">
-          {jobs.map((job, index) => {
-            return (
-              <button
-                onClick={() => setActiveTab(index)}
-                className={`px-5 py-2 w-100 border-b-2 sm:border-b-0 sm:border-l-2 hover:bg-blueGray-800 ${
-                  activeTab === index ? 'border-cyan-300 text-cyan-300 bg-blueGray-800' : 'border-blueGray-700'
-                }`}
-                key={`${job.company}-tabs`}>
-                {job.company}
-              </button>
-            );
-          })}
+        <div className="relative">
+          <div className="flex flex-row sm:flex-col">
+            {jobs.map((job, index) => {
+              return (
+                <button
+                  ref={el => {
+                    if (el !== null) tabsRef.current[index] = el;
+                  }}
+                  onClick={() => setActiveTab(index)}
+                  className={`px-5 py-2 w-100 border-b-2 sm:border-b-0 sm:border-l-2 hover:bg-blueGray-800 border-blueGray-700 ${
+                    activeTab === index && 'text-cyan-300 bg-blueGray-800'
+                  }`}
+                  key={`${job.company}-tabs`}>
+                  {job.company}
+                </button>
+              );
+            })}
+          </div>
+          <span
+            className="absolute sm:w-0.5 sm:left-0 bottom-0 block h-0.5 bg-cyan-300 transition-all duration-300"
+            style={
+              screenWidth < 480
+                ? { left: tabUnderlineLeft, width: tabUnderlineWidth }
+                : { top: tabUnderlineTop, height: '40px' }
+            }
+          />
         </div>
         <div className="pt-2">
           <JobDescription
